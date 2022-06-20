2bb583f6e2ee61d37652bf446dc1acf0b35df26d
DDGUZMANO <douglasguzmanorence29@gmail.com>
2022-06-20 00:14:17 -0300

    Creando 1era entrega del proyecto final

--- js/indexDouglas.js
+++ js/indexDouglas.js
@@ -1,53 +1,8 @@
-// let indice;
-// function imc(peso,estatura){
-//     return imc = (peso / (estatura*estatura));
-// }
-// let peso = +prompt('Ingrese su peso en kilogramos por favor.');
-// let estatura = +prompt('Ingrese su estatura en metros por favor.');
-// indice = imc(peso, estatura);
-// alert('Su indice de masa corporal es '+ indice + '.');
-// if(indice<18.5){
-//     alert('Usted esta bajo de peso.');
-// }
-// else if(indice>= 18.5 && indice <= 24.9){
-//     alert('Su peso es normal.');
-// }
-// else if(indice>= 25 && indice<= 29.9){
-//     alert('Usted tiene sobrepeso.');
-// }else{
-//     alert('Usted sufre de obesidad.');
-// }
-// if(indice>= 25){
-//     alert('Le recomendamos seguir nuestros planes alimenticios y de entrenamientos, de esa forma obtendrá los siguientes resultados por semana...')
-// }
-// if(indice <= 18.5){
-//     alert('Le recomendamos seguir nuestros planes de alimentación y entrenamientos para que alcance su peso ideal de la forma más saludable posible.')
-// }
-// let pesoInicial = peso;
-// let porcentajeDePeso;
-
-// if(indice <= 18.5){
-//     for(let i = 1 ; i <= 8 ; i++){
-//     porcentajeDePeso = pesoInicial + (pesoInicial * 0.015*i);
-//     alert("Luego de la semana " + i + ", su peso será " + porcentajeDePeso + "Kg.");
-// }
-// }
-
-// if(indice >25 ){
-//     for(let i = 1 ; i <= 8 ; i++){
-//     porcentajeDePeso = pesoInicial - (pesoInicial * 0.015*i);
-//     alert("Luego de la semana " + i + ", su peso será " + porcentajeDePeso + "Kg.");
-// }
-// }
-// if(indice > 18.5 && indice <= 24.9){
-//     alert('Su peso es ideal, acá encontraras los mejores tips para seguir evolucionando.');
-// }
-
-//const suplemento = ["wheyprotein" , "massgain" , "fatburner" , "prework"];
+const suplemento = ["wheyprotein" , "massgain" , "fatburner" , "prework"];
 const suplementos = [
-    { codigo: 001, producto: 'wheyprotein', precio: '$25'},
-    { codigo: 002, producto: 'massgain', precio: '$20'},
-    { codigo: 003, producto: 'fatburner', precio: '$35'}
+    { codigo: 101, producto: 'WHEY PROTEIN MAX', precio: '$25'},
+    { codigo: 202, producto: 'MASS GAIN PRO', precio: '$20'},
+    { codigo: 303, producto: 'FAT BURNER PREMIUM', precio: '$35'}
 ]
 for (let i = 0 ; i < suplementos.length; i++){
     console.log(suplementos[i]);
@@ -61,3 +16,66 @@ console.log(suplementos);
 for (const suplemento of suplementos){
     console.log(suplemento);
 };
+
+
+alert('Hola, acá le ayudaremos a controlar su peso');
+
+let nombre = prompt('Ingrese su nombre por favor');
+
+
+
+let peso = +prompt(`Hola ${nombre}, Ingresa tu peso en kilogramos por favor.`);
+
+let estatura = +prompt(`${nombre} ingresa tu estatura en metros por favor.`)
+let indice;
+function imc(peso,estatura){
+    return imc = (peso / (estatura*estatura));
+}
+// let peso = +prompt('Ingrese su peso en kilogramos por favor.');
+// let estatura = +prompt('Ingrese su estatura en metros por favor.');
+indice = imc(peso, estatura);
+alert('Su indice de masa corporal es '+ indice + '.');
+if(indice<18.5){
+    alert('Usted esta bajo de peso.');
+}
+else if(indice>= 18.5 && indice <= 24.9){
+    alert(`Usted tiene un indice de masa corporal normal, le recomendamos probar nuestro poducto ${suplementos[0].producto}, de esta manera podrá fortalecer y tonificar su musculatura de una manera más eficiente. Este producto tiene un costo de USD ${suplementos[0].precio} y lo podrás encontrar en nuestra web con el código ${suplementos[0].codigo}.`);
+}
+else if(indice>= 25 && indice<= 29.9){
+    alert('Usted tiene sobrepeso.');
+}else{
+    alert('Usted sufre de obesidad.');
+}
+if(indice>= 25){
+    alert(`Le recomendamos probar nuestro poducto ${suplementos[2].producto}, de esta manera obtendrás los siguientes resultados...`)
+}
+if(indice <= 18.5){
+    alert(`le recomendamos tomar nuestro producto ${suplementos[1].producto}, de esta forma obtendrás los siguientes resultados...`)
+}
+let pesoInicial = peso;
+let porcentajeDePeso;
+
+if(indice <= 18.5){
+    for(let i = 1 ; i <= 8 ; i++){
+    porcentajeDePeso = pesoInicial + (pesoInicial * 0.015*i);
+    alert("Luego de la semana " + i + ", su peso será " + porcentajeDePeso + "Kg.");
+}
+}
+if(indice <= 18.5){
+    alert(`Este producto tiene un costo de ${suplementos[1].precio} y lo podrás encontrar en nuestra web con el código ${suplementos[1].codigo}.`)
+}
+
+if(indice >25 ){
+    for(let i = 1 ; i <= 8 ; i++){
+    porcentajeDePeso = pesoInicial - (pesoInicial * 0.015*i);
+    alert("Luego de la semana " + i + ", su peso será " + porcentajeDePeso + "Kg.");
+}
+}
+if(indice >25 ){
+    alert(`Este producto tiene un costo de ${suplementos[2].precio} y lo podrás encontrar en nuestra web con el código ${suplementos[2].codigo}.`)
+}
+if(indice > 18.5 && indice <= 24.9){
+    alert('Su peso es ideal, acá encontraras los mejores tips para seguir evolucionando.');
+}
+
+
